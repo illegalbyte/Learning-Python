b79376692a1f2c6c04a9f5e77376be6c6087e041
Lewis Gentle <lewis.gentle@gmail.com>
2021-08-05 23:21:42 +1000

    added r mode which tests all numbers below an integer

--- collatz-sequence.py
+++ collatz-sequence.py
@@ -8,13 +8,25 @@ def collatz(number):
         print(str(3 * int(number) + 1))
         return 3 * int(number) + 1
 
-while True:
-    try:
-        userInput = int(input("Enter a number: "))
 
-        while userInput != 1:
-            userInput = collatz(userInput)
-    except ValueError:
-        print('Invalid input, enter an integer.')
-    except KeyboardInterrupt:
-        raise sys.exit()
+version = input("(r) or (input)?")
+testNumber = 500000000000000000000000000000000000000000000000000000000000000000
+
+if version == 'input':
+    while True:
+        try:
+            userInput = int(input("Enter a number: "))
+
+            while userInput != 1:
+                userInput = collatz(userInput)
+        except ValueError:
+            print('Invalid input, enter an integer.')
+        except KeyboardInterrupt:
+            raise sys.exit(0)
+else:
+    while testNumber != 1:
+        testingInt = testNumber
+        while testingInt != 1:
+            testingInt = collatz(testingInt)
+        else:
+            testNumber = testNumber - 1
