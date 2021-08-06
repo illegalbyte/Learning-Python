643da7b0bd86ec2b4cf5be1623f0c62a96b9cc59
Lewis Gentle <lewis.gentle@gmail.com>
2021-08-06 22:26:22 +1000

    Recursive mode and documentation

--- collatz-sequence.py
+++ collatz-sequence.py
@@ -1,18 +1,32 @@
 import sys
 
+# collatz sequence takes any integer and works its way to one, using only two operations:
+# if the integer if even it is divided by two
+# if the integer is odd it's multiplied by 3 and 1 is added
+# This program can either run recursively, starting with a large number and testing every
+# lower number. Or run once based on a user entered number.
+
+
+# Global Variables:
+    # For the automated version this is the number which collatz() will start at.
+testNumber = int(input("Enter a starting integer: "))
+version = input("(r)ecursive or (i)nput? ")
+
+# FUNCTIONS:
+    # COLLATZ() takes any integer and works its way to 1.
 def collatz(number):
     if (int(number) % 2 == 0):
-        print(str(int(number) // 2))
+        # print(str(int(number) // 2))
         return int(number) // 2
     else:
-        print(str(3 * int(number) + 1))
+        # print(str(3 * int(number) + 1))
         return 3 * int(number) + 1
 
 
-version = input("(r) or (input)?")
-testNumber = 500000000000000000000000000000000000000000000000000000000000000000
 
-if version == 'input':
+
+
+if version == 'i':
     while True:
         try:
             userInput = int(input("Enter a number: "))
@@ -29,4 +43,5 @@ else:
         while testingInt != 1:
             testingInt = collatz(testingInt)
         else:
-            testNumber = testNumber - 1
+            print(str(testNumber) + " Works")
+            testNumber += - 1
