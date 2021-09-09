7cec77801c6a18606ca1b54f7ed8f0cb3680b7d5
Lewis Gentle <lewis.gentle@gmail.com>
2021-09-09 23:10:32 +1000

    Update getMovieData.py

--- getMovieData.py
+++ getMovieData.py
@@ -1,14 +1,27 @@
 #! python3
-# getOpenWeather.py – Gets the weather for any city using city name and country using an API
 
 import json, requests, sys, logging
 from keys import *
 from pprint import pprint
 
 
+
+
 logging.basicConfig(level=logging.WARNING, format=' %(asctime)s - %(levelname)s - %(message)s')
 # logging.disable()
 logging.debug('Start of program')
+
+
+# # TMDB API INTERFACE CLASS
+# class TMDB:
+
+# 	def GetMovieData(TMDB_ID):
+# 		url = f'https://api.themoviedb.org/3/movie/{TMDB_ID}?api_key={TMDB_API_KEY}&language=en-US'
+
+# 	def GetTVData(TMDB_ID):
+
+
+'''
 
 # Get location from command line args
 if len (sys.argv) < 2:
@@ -16,9 +29,11 @@ if len (sys.argv) < 2:
 	sys.exit()
 
 TMDB_ID = ''.join(sys.argv[1])
+'''
+TMDB_ID = '100'
 
 
-# Download the JSON data from Openweathermp.org API
+# Download the JSON data from TMDB API
 url = f'https://api.themoviedb.org/3/movie/{TMDB_ID}?api_key={TMDB_API_KEY}&language=en-US'
 logging.debug(f'URL: {url}')
 
@@ -26,6 +41,7 @@ logging.debug(f'URL: {url}')
 response = requests.get(url)
 response.raise_for_status()
 
+print(response)
 logging.debug(f'TMDB RESPONSE TEXT: {response.text}')
 
 # load JSON into python dictionary:
